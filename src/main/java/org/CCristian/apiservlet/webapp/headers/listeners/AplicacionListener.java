edc834eb45cdaefd99ebd420f0becbcd2a7a52f4
CCrisstian <crisstiann.c@gmail.com>
2024-07-20 19:53:46 -0300

    comenzando la Unidad 46

--- src/main/java/org/CCristian/apiservlet/webapp/headers/listeners/AplicacionListener.java
+++ src/main/java/org/CCristian/apiservlet/webapp/headers/listeners/AplicacionListener.java
@@ -1,4 +1,46 @@
 package org.CCristian.apiservlet.webapp.headers.listeners;
 
-public class AplicacionListener {
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.ServletRequestEvent;
+import jakarta.servlet.ServletRequestListener;
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+
+@WebListener
+public class AplicacionListener implements ServletContextListener,
+        ServletRequestListener, HttpSessionListener {
+
+    /*ServletContextListener*/
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().log("Inicializando la Aplicación");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        sce.getServletContext().log("Destruyendo la Aplicación");
+    }
+
+
+    /*ServletRequestListener*/
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+    }
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+    }
+
+
+    /*ServletRequestListener*/
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+    }
 }
